77c1245d3c20815e2ebf947afcfb4ae2a00c6b34
Toby Hutchinson <tobyhutchinson@gmail.com>
2025-12-17 19:17:45 +0000

    Doing more research

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Assembler
 The purpose of this project is to learn a bit of assembly and how assemblers work by writing one myself in Python. I have written a simple assembler as part of the NAND2Tetris project but this was very simplified. I'm interested in learning about real-world instruction sets and how they get assembled. 
 
+### RISC-V
+I am going to make a RISC-V assembler. Specifically a RV32I assembler. RISC-V is a free and open source ISA, as opposed to ARM and x86 which are licensed. There are many extensions of the base ISA, however I want to build an assembler for the RV32I base instruction set. 
+
+### RV32I
+- RV32I is the 32-bit base integer instruction set (no floating point operations). 
+- It has 40 instructions. 
+- It's little-endian. 
+- I will be using GNU assembler syntax.
+- Instructions are aligned on 4-byte boundaries (misalignment causes exception). 
+
+### Resources
+- [Comprehensive Guide to 32-Bit RISC-V Assembly Programming](https://gist.github.com/robert-saramet/1b9ef3cac5a8345c90d84ac1ac4a8d2b)
+- [RV32I Cheat Sheet](https://www.vicilogic.com/static/ext/RISCV/RV32I_BaseInstructionSet.pdf)
+
 
--- assembler.py
+++ assembler.py
@@ -1,22 +1,9 @@
-from enum import Enum
 import os
 import sys
 import argparse
 from pathlib import Path
 import json
-from enum import Enum
-
-class TokenType(Enum):
-    Instruction = 0
-    Directive = 1
-    Label = 2
-
-class Token:
-    def __init__(self, type, value, line_num, args=None):
-        self.type = type
-        self.value = value
-        self.line_num = line_num
-        self.args = args
+from parser import Parser
 
 class Assembler:
     def __init__(self, verbose=False):
@@ -24,7 +11,6 @@ class Assembler:
         self._source = {}                           # Contains info on all source code encountered
         self._PC = 0
         self._cur_file = None
-        self._machine_code = []                     # Have a mega buffer that stores all output instructions
         self._output_file_path = None
         self.verbose = verbose
 
@@ -46,8 +32,7 @@ class Assembler:
 
         self._verbose_print(f"Output file set to {self._output_file_path}")
 
-        # Parse (2 passes)
-        self._parse_source_file(source_file)   
+        self.parser = Parser(source_file)
 
         # Extract labels
         self._pass_one()
@@ -55,198 +40,18 @@ class Assembler:
         # Translate instructions to binary
         self._pass_two()
 
-        print("Compilation Successful.")
-        
-    def _error(self, line_num, msg, error_code):
-        print(f"{self._cur_file}:{line_num}: ERROR: {msg}")
-        sys.exit(error_code)
-
-    def _parse_source_file(self, source_file):
-        try:
-            with open(source_file, 'r') as file:
-                file_name = os.path.basename(source_file)
-                if file_name in self._source:
-                    print(f"ERROR: {file_name} already parsed?!")
-                    sys.exit(-1)
-
-                self._verbose_print(f"Parsing {source_file}...")
-                
-                self._source[file_name] = {}
-                self._source[file_name]["instructions"] = []
-                self._source[file_name]["parent_file"] = self._cur_file     # Top-level file has no parent
-                self._cur_file = file_name
-
-                # Clean the lines
-                for i, line in enumerate(file):
-                    line = line.strip()
-                    if line == '' or line.startswith("//"):
-                        continue
-                    else:
-                        if ("//" in line):
-                            line = line[:line.index("//")]      # Remove comments
-
-                        # Tokeniser
-                        tokens = [ el for el in line.split(" ") if el != "" ]   
-
-                        print(tokens)
-
-                        # Handle <label> -> command
-                        """
-                        f_token = tokens.pop(0)
-
-                        if self._token_is_label(tokens[0]):
-                            token = Token(
-                                type=TokenType.Label,
-                                value=tokens[0]
-                            )
-                            self._source[self._cur_file]["tokens"].append(token)
-                        """
-        except IOError as e:
-            print(f"ERROR: Unable to open source file '{source_file}'")
-            sys.exit(1)
-
-
-    def _token_is_label(self, token):
-        return  token.endswith(':') and (token[0].isalpha() or token.startswith('_'))
-
-    def _token_is_directive(self, token):
-        return token.startswith('.')
-
-    def _calc_padding(self, tokens, line_num):
-        alignment = 4           # Default 2**2
-        if len(tokens) == 2:
-            power = tokens[1]
-            try:
-                alignment = 2**int(power)
-            except ValueError:
-                self._error(line_num, f"Expected integer, got {power}", -1)
-
-        return (alignment - (self._PC % alignment)) % alignment
-
+        # Output binary
 
     def _pass_one(self):
         self._verbose_print(f"=== Starting first pass for {self._cur_file} ===")
-
-        for token in self._source[self._cur_file]["tokens"]:
-            if token.type == TokenType.Label:
-                label_name = token.value[:-1]
-                if not label_name in self._labels:
-                    self._labels[label_name] = self._PC
-                    token_index = 1
-                else:
-                    self._error(token.line_num, f"Symbol '{label_name}' already defined.", -1)
-
-            # === Handle Directives ===
-            elif token.type == TokenType.Directive:
-                directive = token.value.upper()
-                self._verbose_print(f"Handling directive: {directive}")
-                match directive:
-                    # === CONTROL DIRECTIVES ===
-                    case ".INCLUDE":    
-                        # .include {file}
-                        if len(tokens) != 2:
-                            self._error(ins["line_num"], "Syntax: '.INCLUDE {filename}'.", -1)
-                        
-                        # We need to recursively assemble the files
-                        source_file = tokens[token_index+1]
-                        self.assemble(source_file)
-                        self._cur_file = self._source[self._cur_file]["parent_file"]
-
-                    case ".ALIGN" | ".BALIGN":  # MVP
-                        # .align {expression,{offset-expression}}
-                        # I will ignore offset expression
-                        if len(tokens) > 2:
-                            self._error(ins["line_num"], "Syntax: '.ALIGN {expression}'", -1)
-
-                        self._PC += self._calc_padding(tokens, ins["line_num"])
-
-                        
-                    case ".TEXT":
-                        continue
-                    case ".DATA":
-                        continue
-
-                    # === Symbol Directives ===
-                    case ".EQU" | ".SET":
-                        continue
-                    case ".GLOBAL" | ".GLOBL":  # This is for the linker...
-                        continue
-
-                    # === Constant Definition Directives ===
-                    case ".BYTE":   # MVP
-                        for token in tokens[1:]:
-                            # Assume they're the right size for now...
-                            self._PC += 1
-
-                    case ".WORD" | ".INT" | ".LONG":    # MVP
-                        for token in tokens[1:]:
-                            # Assume they're the right size for now...
-                            self._PC += 4   # 4-Bytes
-
-                    case _:
-                        self._error(ins["line_num"], f"This directive doesn't exist or hasn't been implemented yet: {directive}", -1)
-
-            else:   # Assume normal instructions
-                self._PC += 4
 
     def _pass_two(self):
         # Turn assembly to binary...
         self._verbose_print(f"=== Starting first pass for {self._cur_file} ===")
 
-        self._PC = 0
-        self._output_buffer = bytearray()
-
-        for ins in self._source[self._cur_file]["instructions"]:
-            tokens = ins["tokens"]
-            
-            # If first word in label, remove it
-            if self._token_is_label(tokens[0]):
-                tokens = tokens[1:]
-
-            if len(tokens) == 0:
-                continue
-            
-            if self._token_is_directive(tokens[0]):
-                directive = tokens[0].upper()
-                
-                match directive:
-                    # === CONTROL DIRECTIVES ===
-                    case ".INCLUDE":    
-                        continue    # Handled in first pass
-
-                    case ".ALIGN" | ".BALIGN": 
-                        if len(tokens) > 2:
-                            self._error(ins["line_num"], "Syntax: '.ALIGN {expression}'", -1)
-
-                        padding = self._calc_padding(tokens, ins["line_num"])
-
-                        if padding != 0:
-                            self._output_buffer.extend(bytearray.fromhex('00'*padding))
-                            self._PC += padding
-                            self._verbose_print(f"Alignment: Padded {padding} bytes (PC now at 0x{self._PC:X})")
-                        
-                    # ** Ignoring these for now
-                    case ".TEXT":
-                        continue
-                    case ".DATA":
-                        continue
-                    # === Symbol Directives ===
-                    case ".EQU" | ".SET":
-                        continue
-                    case ".GLOBAL" | ".GLOBL":  # This is for the linker...
-                        continue
-
-                    # === Constant Definition Directives ===
-                    case ".BYTE":
-                        continue
-
-                    case ".WORD" | ".INT" | ".LONG":    # MVP
-                        for token in tokens[1:]:
-                            # Assume they're the right size for now...
-                            self._PC += 4   # 4-Bytes
-
-                    case _:
-                        self._error(ins["line_num"], f"This directive doesn't exist or hasn't been implemented yet: {directive}", -1)
+    def _error(self, line_num, msg, error_code):
+        print(f"{self._cur_file}:{line_num}: ERROR: {msg}")
+        sys.exit(error_code)
 
 
 def main():
